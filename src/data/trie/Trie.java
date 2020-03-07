6be87cf03782b9933442e59d7f1236d57be0df0b
liyuzhan <1031759184@qq.com>
2020-03-07 11:28:56 +0800

    新增trie字典树

---
+++ src/data/trie/Trie.java
@@ -0,0 +1,63 @@
+package data.trie;
+
+import java.util.TreeMap;
+
+/**
+ * @Author: liyuzhan
+ * @classDesp： 字典树实现类
+ * @Date: 2020/3/7 11:07
+ * @Email: 1031759184@qq.com
+ */
+public class Trie {
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    /**
+     * 获得Trie中存储的单词数量
+     *
+     * @return 单词数量
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 向Trie中添加一个新的单词word
+     *
+     * @param word 单词
+     */
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord){
+            cur.isWord = true;
+            size++;
+        }
+
+    }
+}
