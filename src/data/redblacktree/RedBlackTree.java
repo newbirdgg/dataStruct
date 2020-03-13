35d1a3d41110422c38cb37f3879fe38d32dbe195
liyuzhan <1031759184@qq.com>
2020-03-13 19:32:42 +0800

    新增红黑树

---
+++ src/data/redblacktree/RedBlackTree.java
@@ -0,0 +1,249 @@
+package data.redblacktree;
+
+import data.map.BinarySearchTreeMap;
+
+import java.util.Random;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * @Author: liyuzhan
+ * @classDesp： 左倾 红黑树基于二分搜索树的底层实现
+ * @Date: 2020/3/12 10:35
+ * @Email: 1031759184@qq.com
+ */
+public class RedBlackTree<K extends Comparable<K>, V> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node left, right;
+        //节点颜色
+        public boolean color;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+            color = RED;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+
+    public RedBlackTree() {
+        root = null;
+        size = 0;
+    }
+
+    /**
+     * 判断节点颜色是否是红色
+     *
+     * @param node 传入节点
+     * @return 是否是红色
+     */
+    private boolean isRed(Node node) {
+        if (node == null) {
+            return BLACK;
+        }
+        return node.color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 左倾红黑树左旋转
+     * @param node 传入几点
+     * @return 旋转后产生的新的根节点
+     */
+    private Node leftRotate(Node node) {
+        Node x = node.right;
+        //左旋转
+        node.right = x.left;
+        x.left = node;
+        x.color = node.color;
+        node.color = RED;
+        return x;
+    }
+
+    /**
+     * 左倾红黑树右旋转
+     * @param node 传入节点
+     * @return 旋转后产生的新的根节点
+     */
+    private Node rightRotate(Node node){
+        Node x = node.left;
+        //右旋转
+        node.left = x.right;
+        x.right = node;
+        x.color = node.color;
+        node.color = RED;
+
+        return x;
+    }
+
+    /**
+     * 左倾红黑树颜色翻转
+     * @param node 传入节点
+     */
+    private void flipColor(Node node){
+        node.color = RED;
+        node.left.color = BLACK;
+        node.right.color = BLACK;
+    }
+
+    // 向红黑树中添加新的元素(key, value)
+    public void add(K key, V value) {
+        root = add(root, key, value);
+        //最终根节点为黑色节点
+        root.color = BLACK;
+    }
+
+    // 向以node为根的红黑树中插入元素(key, value)，递归算法
+    // 返回插入新节点后红黑树的根
+    private Node add(Node node, K key, V value) {
+
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+
+        if (key.compareTo(node.key) < 0) {
+            node.left = add(node.left, key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = add(node.right, key, value);
+        } else // key.compareTo(node.key) == 0
+        {
+            node.value = value;
+        }
+
+        return node;
+    }
+
+    // 返回以node为根节点的二分搜索树中，key所在的节点
+    private Node getNode(Node node, K key) {
+
+        if (node == null) {
+            return null;
+        }
+
+        if (key.equals(node.key)) {
+            return node;
+        } else if (key.compareTo(node.key) < 0) {
+            return getNode(node.left, key);
+        } else // if(key.compareTo(node.key) > 0)
+        {
+            return getNode(node.right, key);
+        }
+    }
+
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
+    }
+
+    public V get(K key) {
+
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+    public void set(K key, V newValue) {
+        Node node = getNode(root, key);
+        if (node == null) {
+            throw new IllegalArgumentException(key + " doesn't exist!");
+        }
+
+        node.value = newValue;
+    }
+
+    // 返回以node为根的二分搜索树的最小值所在的节点
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMin(Node node) {
+
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    // 从二分搜索树中删除键为key的节点
+    public V remove(K key) {
+
+        Node node = getNode(root, key);
+        if (node != null) {
+            root = remove(root, key);
+            return node.value;
+        }
+        return null;
+    }
+
+    private Node remove(Node node, K key) {
+
+        if (node == null) {
+            return null;
+        }
+
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+            return node;
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+            return node;
+        } else {   // key.compareTo(node.key) == 0
+
+            // 待删除节点左子树为空的情况
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+
+            // 待删除节点右子树为空的情况
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+            // 待删除节点左右子树均不为空的情况
+
+            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+            // 用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+
+            return successor;
+        }
+    }
+
+
+}
