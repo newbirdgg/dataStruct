7e5df7bc34e0458a4cf2cab7f56f93f76af165f0
liyuzhan <1031759184@qq.com>
2020-03-20 20:53:33 +0800

    新增递归题解

---
+++ src/data/recursive/Solution1.java
@@ -0,0 +1,24 @@
+package data.recursive;
+/**
+ *  @Author: liyuzhan
+ *  @classDesp： 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0
+ *  @Date: 2020/3/20 10:02
+ *  @Email: 1031759184@qq.com
+ */
+public class Solution1 {
+    public int Fibonacci(int n) {
+        if (n==0){
+            return 0;
+        }
+        if (n<=2){
+            return 1;
+        }
+        return Fibonacci(n-1)+Fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+        int n = 39;
+        Solution1 solution1 = new Solution1();
+        System.out.println(solution1.Fibonacci(n));
+    }
+}
